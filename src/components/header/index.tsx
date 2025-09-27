f2b58bead3804054e2e9b2e3a7a967c7f6f07912
Kaio <kposansky@gmail.com>
2025-09-27 12:23:12 -0300

    estrutura nextjs

---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+export function Header(){
+    return(
+        <header>
+            <div>
+                <div>
+                    <h1>Biblioteka</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>Home</li>
+
+                        <li>Biblioteca</li>
+
+                        <li>Emprestimo</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
