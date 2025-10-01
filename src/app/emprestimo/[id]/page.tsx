f7e8a183ffbfff089c2e8e71a0e97bbbaa973a3e
Kaio <kposansky@gmail.com>
2025-10-01 01:47:34 -0300

    feat: checagem de emprestimo e multa de emprestimo
    - Checagem de empréstimo por usuário, checa na api backend Java
    - Página para ver emprétimos
    - Página para  ver multa de empréstimo

---
+++ src/app/emprestimo/[id]/page.tsx
@@ -0,0 +1,42 @@
+import {checkSession} from '@/lib/session';
+import {getEmprestimoMulta, EmprestimoMulta} from "@/services/emprestimo";
+import EmprestimoMultaItem from "@/components/emprestimo/emprestimoMulta";
+
+export default async function EmprestimoPage({params}: {
+    params: Promise<{ id: string }>
+}) {
+    const session = await checkSession();
+    const { id } = await params;
+    const numericId = parseInt(id);
+    const result : EmprestimoMulta | null = await getEmprestimoMulta(numericId);
+
+    const multa =
+        result != null ?
+        (
+        <EmprestimoMultaItem
+            key={result.loanFineId}
+            bookTitle={result.bookTitle}
+            authorName={result.authorName}
+            loanDate={result.loanDate}
+            dueDate={result.dueDate}
+            returnDate={result.returnDate}
+            costPerDay={result.costPerDay}
+            payed={false}
+        />
+    ) :
+        <p className={"flex justify-center text-lg"}>
+            Multa não encontrada!
+        </p>
+    ;
+
+    return (
+        <div className={"flex justify-center w-full"}>
+            <div className={"flex flex-col items-center gap-y-10"}>
+                <h1 className={"text-3xl font-bold text-emerald-700"}>Informações de multa do empréstimo</h1>
+                <div className={"flex flex-col items-center gap-y-5"}>
+                    {multa}
+                </div>
+            </div>
+        </div>
+    );
+}
